40ceaf83e6af49eba2781c6b1543ae59f8d6afa1
phuctruong.meu <phuctruong@meu-solutions.com>
2020-10-15 12:58:49 +0700

    remove unuse file

--- Recipes/Module/Recipe/Scene/Recipes/RecipesViewController.swift
+++ Recipes/Module/Recipe/Scene/Recipes/RecipesViewController.swift
@@ -34,28 +34,3 @@ class RecipesViewController: UIViewController {
         .disposed(by: disposeBag)
     }
 }
-
-class ImageLoader {
-
-  private static let cache = NSCache<NSString, NSData>()
-
-  class func image(for url: URL, completionHandler: @escaping(_ image: UIImage?) -> ()) {
-
-    DispatchQueue.global(qos: DispatchQoS.QoSClass.background).async {
-
-      if let data = self.cache.object(forKey: url.absoluteString as NSString) {
-        DispatchQueue.main.async { completionHandler(UIImage(data: data as Data)) }
-        return
-      }
-
-      guard let data = NSData(contentsOf: url) else {
-        DispatchQueue.main.async { completionHandler(nil) }
-        return
-      }
-
-      self.cache.setObject(data, forKey: url.absoluteString as NSString)
-      DispatchQueue.main.async { completionHandler(UIImage(data: data as Data)) }
-    }
-  }
-
-}
